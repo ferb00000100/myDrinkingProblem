ae5dedd8d56f75c4aa58af124016197c0dcecab4
ferb00000100 <fhlh@protonmail.com>
2019-10-19 12:04:32 -0600

    added 50 states; ajax query with results into an object

--- assests/script.js
+++ assests/script.js
@@ -22,116 +22,41 @@
 	var byTag = 'by_tag';
 	var byTags = 'by_tags';
 	var selection = [];
-	var baseBrewerydbURL ="https://api.openbrewerydb.org/breweries?";
-	// var baseBrewerydbURL ="https://api.openbrewerydb.org/breweries?by_city=denver";
-	var brewerydb = baseBrewerydbURL;
+	// var baseBrewerydbURL ="https://api.openbrewerydb.org/breweries?";
+	// var brewerydb = baseBrewerydbURL;
 
 
+	function outputResults() {
 
-	$.ajax({
-		url: brewerydb,
+
+	}
+
+	function getResults(selectedState){
+		var baseBrewerydbURL ="https://api.openbrewerydb.org/breweries?by_state="+selectedState;
+		$.ajax({
+		url: baseBrewerydbURL,
 		method: "GET"
-	}).then(function (response) {
-		console.log(response);
-		response = JSON.stringify((response));
-		$('#city').text(response);
-		// for ( var i = 0; i < 10; i++){
-		// console.log(response.restaurants[i]);
-		// console.log(response.restaurants[i].restaurant.name);
-		// console.log(response.restaurants[i].establishment[i]);
-		// }
-	});
+		}).then(function (response) {
+			console.log(response);
+			outputResults = {
+				"Name": response[0].name,
+				"Phone Number": response[0].phone,
+				"State": response[0].state,
+				"Address": response[0].street,
+				"Web URL": response[0].website_url
+			}
+		console.log(outputResults);
+		// response = JSON.stringify((response));
+		});
+	}
 
-$('.demo').dropkick({
+	function getState() {
+		var selectedState = $( "select#states" ).val();
+		// console.log(selectedState);
+		getResults(selectedState);
+	}
 
-	/**
-	 * Called once after the DK element is inserted into the DOM.
-	 * The value of `this` is the Dropkick object itself.
-	 *
-	 * @config initialize
-	 * @type Function
-	 *
-	 */
-	initialize() {},
+$( "select" ).change( getState );
+// getState();
 
-	/**
-	 * Whether or not you would like Dropkick to render on mobile devices.
-	 *
-	 * @default false
-	 * @property {boolean} mobile
-	 * @type boolean
-	 *
-	 */
-	mobile: true,
 
-	/**
-	 * Called whenever the value of the Dropkick select changes (by user action or through the API).
-	 * The value of `this` is the Dropkick object itself.
-	 *
-	 * @config change
-	 * @type Function
-	 *
-	 */
-	change() {},
-
-	/**
-	 * Called whenever the Dropkick select is opened. The value of `this` is the Dropkick object itself.
-	 *
-	 * @config open
-	 * @type Function
-	 *
-	 */
-	open() {},
-
-	/**
-	 * Called whenever the Dropkick select is closed. The value of `this` is the Dropkick object itself.
-	 *
-	 * @config close
-	 * @type Function
-	 *
-	 */
-	close() {},
-
-	// Search method; "strict", "partial", or "fuzzy"
-	/**
-	 * `"strict"` - The search string matches exactly from the beginning of the option's text value (case insensitive).
-	 *
-	 * `"partial"` - The search string matches part of the option's text value (case insensitive).
-	 *
-	 * `"fuzzy"` - The search string matches the characters in the given order (not exclusively).
-	 * The strongest match is selected first. (case insensitive).
-	 *
-	 * @default "strict"
-	 * @config search
-	 * @type string
-	 *
-	 */
-	search: "strict",
-
-	/**
-	 * Bubble up the custom change event attached to Dropkick to the original element (select).
-	 */
-	bubble: true
-
-});
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-// function myFunction() {
-// 	$('#dropdown').on(ontoggle)
-// 	document.getElementById("myDropdown").classList.toggle("show");
-// }
-
-// Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-// 	if (!event.target.matches('.dropbtn')) {
-// 		var dropdowns = document.getElementsByClassName("dropdown-content");
-// 		var i;
-// 		for (i = 0; i < dropdowns.length; i++) {
-// 			var openDropdown = dropdowns[i];
-// 			if (openDropdown.classList.contains('show')) {
-// 				openDropdown.classList.remove('show');
-// 			}
-// 		}
-// 	}
-// }
