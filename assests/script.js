f1e2bba8588f782cb04c7033a26cfaf16af164e9
ferb00000100 <fhlh@protonmail.com>
2019-10-19 10:56:41 -0600

    added dropdown and 50 states

--- assests/script.js
+++ assests/script.js
@@ -22,8 +22,8 @@
 	var byTag = 'by_tag';
 	var byTags = 'by_tags';
 	var selection = [];
-	// var baseBrewerydbURL ="https://api.openbrewerydb.org/breweries?";
-	var baseBrewerydbURL ="https://api.openbrewerydb.org/breweries?by_city=denver";
+	var baseBrewerydbURL ="https://api.openbrewerydb.org/breweries?";
+	// var baseBrewerydbURL ="https://api.openbrewerydb.org/breweries?by_city=denver";
 	var brewerydb = baseBrewerydbURL;
 
 
@@ -42,9 +42,83 @@
 		// }
 	});
 
+$('.demo').dropkick({
+
+	/**
+	 * Called once after the DK element is inserted into the DOM.
+	 * The value of `this` is the Dropkick object itself.
+	 *
+	 * @config initialize
+	 * @type Function
+	 *
+	 */
+	initialize() {},
+
+	/**
+	 * Whether or not you would like Dropkick to render on mobile devices.
+	 *
+	 * @default false
+	 * @property {boolean} mobile
+	 * @type boolean
+	 *
+	 */
+	mobile: true,
+
+	/**
+	 * Called whenever the value of the Dropkick select changes (by user action or through the API).
+	 * The value of `this` is the Dropkick object itself.
+	 *
+	 * @config change
+	 * @type Function
+	 *
+	 */
+	change() {},
+
+	/**
+	 * Called whenever the Dropkick select is opened. The value of `this` is the Dropkick object itself.
+	 *
+	 * @config open
+	 * @type Function
+	 *
+	 */
+	open() {},
+
+	/**
+	 * Called whenever the Dropkick select is closed. The value of `this` is the Dropkick object itself.
+	 *
+	 * @config close
+	 * @type Function
+	 *
+	 */
+	close() {},
+
+	// Search method; "strict", "partial", or "fuzzy"
+	/**
+	 * `"strict"` - The search string matches exactly from the beginning of the option's text value (case insensitive).
+	 *
+	 * `"partial"` - The search string matches part of the option's text value (case insensitive).
+	 *
+	 * `"fuzzy"` - The search string matches the characters in the given order (not exclusively).
+	 * The strongest match is selected first. (case insensitive).
+	 *
+	 * @default "strict"
+	 * @config search
+	 * @type string
+	 *
+	 */
+	search: "strict",
+
+	/**
+	 * Bubble up the custom change event attached to Dropkick to the original element (select).
+	 */
+	bubble: true
+
+});
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 // function myFunction() {
+// 	$('#dropdown').on(ontoggle)
 // 	document.getElementById("myDropdown").classList.toggle("show");
 // }
 
